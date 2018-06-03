c6e0d118b28a8ad0b6d0d8ea38b10f6d34649b18
CoziestYew804 <enas1@hotmail.fr>
2018-06-03 20:24:23 +0200

    Oubli d'ajouter le gameOver Screen

---
+++ core/src/com/ul/game/screens/GameOverScreen.java
@@ -0,0 +1,78 @@
+package com.ul.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.ul.game.PacManGdx;
+import com.ul.game.model.elements.GameElement;
+import com.ul.game.view.TextureFactory;
+import com.badlogic.gdx.Game;
+
+public class GameOverScreen implements Screen {
+    private Texture gameOverLogo;
+    private SpriteBatch spriteBatch;
+    private Game game;
+
+    public GameOverScreen(Game game){
+        gameOverLogo = new Texture("core/assets/gameover_04.jpg");
+        spriteBatch= new SpriteBatch();
+        this.game=game;
+
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        this.spriteBatch.begin();
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        this.spriteBatch.draw(gameOverLogo,
+                70,
+                110,
+                300,
+                300
+        );
+        this.spriteBatch.end();
+
+
+        if (Gdx.input.isTouched()) {
+            //game.setScreen(new HomeScreen(game));
+            //float y = this.gs.getMonde().getPacman().getPosition().y;
+
+        }
+    }
+
+
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
