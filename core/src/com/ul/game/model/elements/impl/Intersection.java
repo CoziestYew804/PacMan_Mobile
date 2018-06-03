742e768a9606e7dd5d2c2d15fe90eeacf52044d3
CoziestYew804 <enas1@hotmail.fr>
2018-06-03 16:07:35 +0200

    Les ghost are perfect ;)

--- core/src/com/ul/game/model/elements/impl/Intersection.java
+++ core/src/com/ul/game/model/elements/impl/Intersection.java
@@ -39,19 +39,23 @@ public class Intersection extends Dark {
     }
     public Vector2 getBestPossibilitieTo(GameElement element){
         Vector2 temp=new Vector2(0,0);
+        Vector2 cible;
+        Vector2 maPosition;
         for (Vector2 direction: possibleDirection) {
             if(!this.isNextABlock(direction)&&!this.isNextAGhostDoor(direction)){
                temp=direction;
             }
         }
-        /*for (Vector2 direction: possibleDirection) {
+        for (Vector2 direction: possibleDirection) {
+            cible=new Vector2(element.getPosition());
+            maPosition=new Vector2(this.getPosition());
             if(!this.isNextABlock(direction)&&!this.isNextAGhostDoor(direction)&&!this.isNextABlock(temp)&&!this.isNextAGhostDoor(temp)){
-                    if(element.getPosition().dst(this.getPosition().add(temp)) > element.getPosition().dst(this.getPosition().add(direction))){
+                    if(cible.dst(maPosition.add(temp)) > cible.dst(maPosition.add(direction))){
                         temp=direction;
                 }
 
             }
-        }*/
+        }
 
         return temp;
     }
